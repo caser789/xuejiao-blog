42095a144ef0e6fb2f748414b599c4d8dc1d0f69
John <jiao.xuejiao@gmail.com>
2015-06-28 11:18:45 +0800

    add confirmation

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,9 +1,32 @@
 from flask.ext.wtf import Form
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import Required, Length, Email
+from wtforms.validators import Required, Length, Email, EqualTo, Regexp
+from ..models import User
+from wtforms import ValidationError
+
+
 
 class LoginForm(Form):
     email = StringField('Email', validators=[Required(), Length(1, 64), Email()])
     password = PasswordField('Password', validators=[Required(), Length(1, 64)])
     remember_me = BooleanField('Keep me logged in.')
     submit = SubmitField('Log in')
+
+class RegistrationForm(Form):
+    email = StringField('Email', validators=[Required(), Length(1, 64), Email()])
+    username = StringField('Username', validators=[
+            Required(), Length(1, 64), Regexp('^[A-Za-z][A-Za-z0-9_.]*$', 0, 
+                                              'Usernames must only have'
+                                              'letters, numbers, dots or'
+                                              'unserscores')])
+    password = PasswordField('Password', validators=[Required(), Length(1, 64),
+        EqualTo('password2', message='Passwords must match.')])
+    password2 = PasswordField('Confirm password', validators=[Required()])
+    submit = SubmitField('Submit')
+
+    def validate_email(self, field):
+        if User.query.filter_by(email=field.data).first():
+            raise ValidationError('Email already registered.')
+    def validate_username(self, field):
+        if User.query.filter_by(username=field.data).first():
+            raise ValidationError('Username already in use')
--- app/auth/views.py
+++ app/auth/views.py
@@ -1,8 +1,24 @@
 from flask import render_template, url_for, redirect, request, flash
 from ..models import User
 from . import auth
-from .forms import LoginForm
-from flask.ext.login import login_user, login_required, logout_user
+from .. import db
+from .forms import LoginForm, RegistrationForm
+from flask.ext.login import login_user, login_required, logout_user, current_user
+from ..email import send_email
+
+@auth.before_app_request
+def before_request():
+    if current_user.is_authenticated() \
+            and not current_user.confirmed \
+            and request.endpoint[:5] != 'auth.' \
+            and request.endpoint != 'statis':
+        return redirect(url_for('auth.unconfirmed'))
+
+@auth.route('/unconfirmed')
+def unconfirmed():
+    if current_user.is_anonymous() or current_user.confirmed:
+        return redirect(url_for('main.index'))
+    return render_template('auth/unconfirmed.html')
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
@@ -20,4 +36,40 @@ def login():
 def logout():
     logout_user()
     flash('You have been logged out.')
+    return redirect(url_for('main.index'))
+
+@auth.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email=form.email.data,
+                    username=form.username.data,
+                    password=form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        token = user.generate_confirmation_token()
+        send_email(user.email, 'Confirm Your Account', 
+                   'auth/email/confirm', user=user, token=token)
+        flash('A confirmation letter has been sent to you by email.')
+        return redirect(url_for('.login'))
+    return render_template('auth/register.html', form=form)
+
+@auth.route('/confirm/<token>')
+@login_required
+def confirm(token):
+    if current_user.confirmed:
+        return redirect(url_for('main.index'))
+    if current_user.confirm(token):
+        flash('You have confirmed your account. Thanks!')
+    else:
+        flash('The confirmation link is invalid or expired.')
+    return redirect(url_for('main.index'))
+
+@auth.route('/confirm')
+@login_required
+def resend_confirmation():
+    token = user.generate_confirmation_token()
+    send_email(current_user.email, 'Confirm Your Account', 
+               'auth/email/confirm', user=current_user, token=token)
+    flash('A new confirmation email has been sent to you.')
     return redirect(url_for('main.index'))
