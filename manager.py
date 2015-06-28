1beeec34bc560134b5b5f7fa413cbbc874243b93
John <jiao.xuejiao@gmail.com>
2015-06-28 09:43:27 +0800

    add login and logout

--- manager.py
+++ manager.py
@@ -17,7 +17,7 @@ manager.add_command('db', MigrateCommand)
 def test():
     """Run the unit tests."""
     import unittest
-    tests = unittest.TestLoader().discrover('tests')
+    tests = unittest.TestLoader().discover('tests')
     unittest.TextTestRunner(verbosity=2).run(tests)
 
 if __name__ == '__main__':
