692e3097f9bafb335e8f467dd307c172b661863b
caser789 <jiao.xuejiao@gmail.com>
2015-06-30 22:38:16 +0800

    add role management

--- app/models.py
+++ app/models.py
@@ -1,8 +1,15 @@
 from . import db, login_manager
 from werkzeug.security import generate_password_hash, check_password_hash
-from flask.ext.login import UserMixin
+from flask.ext.login import UserMixin, AnonymousUserMixin
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 from flask import current_app
+
+class Permission:
+    FOLLOW = 0x01
+    COMMENT = 0x02
+    WRITE_ARTICLES = 0x04 
+    MODERATE_COMMENTS = 0x08
+    ADMINISTER = 0x80
 
 
 class Role(db.Model):
@@ -10,6 +17,29 @@ class Role(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), unique=True, index=True)
     users = db.relationship('User', backref='role', lazy='dynamic')
+    default = db.Column(db.Boolean, default=False)
+    permissions = db.Column(db.Integer)
+
+    @staticmethod
+    def insert_roles():
+	roles = {
+	    'User': (Permission.FOLLOW | 
+		     Permission.COMMENT | 
+		     Permission.WRITE_ARTICLES, True),
+	    'Moderator': (Permission.FOLLOW |
+			  Permission.COMMENT |
+			  Permission.WRITE_ARTICLES | 
+			  Permission.MODERATE_COMMENTS, False),
+	    'Administrator': (0xff, False)
+	}
+	for r in roles:
+	    role = Role.query.filter_by(name=r).first()
+	    if role is None:
+		role = Role(name=r)
+	    role.permissions = roles[r][0]
+	    role.default = roles[r][1]
+	    db.session.add(role)
+	db.session.commit()
 
     def __repr__(self):
         return '<Role %r>' % self.name
@@ -21,6 +51,20 @@ class User(UserMixin, db.Model):
     password_hash = db.Column(db.String(128))
     email = db.Column(db.String(64), unique=True, index=True)
     confirmed = db.Column(db.Boolean, default=False)
+
+    def __init__(self, **kw):
+	super(User, self).__init__(**kw)
+	if self.role == None:
+	    if self.email == current_app.config['BLOG_ADMIN']:
+		self.role = Role.query.filter_by(name='Administrator').first()
+	    else:
+		self.role = Role.query.filter_by(default=True).first()
+    def can(self, permissions):
+	return self.role is not None and \
+		(self.role.permissions & permissions) == permissions
+    
+    def is_administrator(self):
+	return self.can(Permission.ADMINISTER)
 
     @property
     def password(self):
@@ -88,6 +132,13 @@ class User(UserMixin, db.Model):
 
     def __repr__(self):
         return '<User %r>' % self.username
+class AnonymousUser(AnonymousUserMixin):
+    def can(self, permission):
+	return False
+
+    def is_administrator(self):
+	return False
+login_manager.anonymous_user = AnonymousUser
 
 @login_manager.user_loader
 def load_user(user_id):
--- tests/test_user_model.py
+++ tests/test_user_model.py
@@ -1,6 +1,6 @@
 import unittest
 from app import create_app, db
-from app.models import User
+from app.models import User, Role, Permission, AnonymousUser
 import time
 
 class UserModelTestCase(unittest.TestCase):
@@ -9,6 +9,9 @@ class UserModelTestCase(unittest.TestCase):
         self.app_context = self.app.app_context()
         self.app_context.push()
         db.create_all()
+	Role.insert_roles()
+
+
     def tearDown(self):
         db.session.remove()
         db.drop_all()
@@ -59,7 +62,7 @@ class UserModelTestCase(unittest.TestCase):
         db.session.commit()
         token = u.generate_reset_token()
         self.assertTrue(u.reset_password(token, 'dog'))
-        sefl.assertTrue(u.verify_password('dog'))
+        self.assertTrue(u.verify_password('dog'))
 
     def test_invalid_reset_token(self):
         u1 = User(password='cat')
@@ -91,10 +94,19 @@ class UserModelTestCase(unittest.TestCase):
 
     def test_duplicate_email_change_token(self):
         u1 = User(email='john@example.com', password='cat')
-        u2 = User(email='susan@example.com', password='dog')
+        u2 = User(email='susan@example.org', password='dog')
         db.session.add(u1)
         db.session.add(u2)
         db.session.commit()
         token = u2.generate_email_change_token('john@example.com')
         self.assertFalse(u2.change_email(token))
         self.assertTrue(u2.email == 'susan@example.org')
+    
+    def test_roles_and_permissions(self):
+	u = User(email='john@example.com', password='cat')
+	self.assertTrue(u.can(Permission.WRITE_ARTICLES))
+	self.assertFalse(u.can(Permission.MODERATE_COMMENTS))
+
+    def test_anonymous_user(self):
+	u = AnonymousUser()
+	self.assertFalse(u.can(Permission.FOLLOW))
