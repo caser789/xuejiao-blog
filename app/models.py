3e86ffa3e8af447dd6983fab5139c0c5e164f917
John <jiao.xuejiao@gmail.com>
2015-07-01 08:07:31 +0800

    user profile model and user.html

--- app/models.py
+++ app/models.py
@@ -3,6 +3,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask.ext.login import UserMixin, AnonymousUserMixin
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 from flask import current_app
+from datetime import datetime
 
 class Permission:
     FOLLOW = 0x01
@@ -51,6 +52,11 @@ class User(UserMixin, db.Model):
     password_hash = db.Column(db.String(128))
     email = db.Column(db.String(64), unique=True, index=True)
     confirmed = db.Column(db.Boolean, default=False)
+    name = db.Column(db.String(64))
+    location = db.Column(db.String(64))
+    about_me = db.Column(db.Text())
+    member_since = db.Column(db.DateTime(), default=datetime.utcnow)
+    last_seen = db.Column(db.DateTime(), default=datetime.utcnow)
 
     def __init__(self, **kw):
 	super(User, self).__init__(**kw)
@@ -65,6 +71,10 @@ class User(UserMixin, db.Model):
     
     def is_administrator(self):
 	return self.can(Permission.ADMINISTER)
+    
+    def ping(self):
+        self.last_seen = datetime.utcnow()
+        db.session.add(self)
 
     @property
     def password(self):
