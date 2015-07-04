4e1cff0b1132ed4022278e3d1bf7ddc420d93830
John <jiao.xuejiao@gmail.com>
2015-07-04 13:50:44 +0800

    api finished

---
+++ app/api_1_0/users.py
@@ -0,0 +1,54 @@
+from flask import jsonify, request, current_app, url_for
+from . import api
+from ..models import User, Post
+
+# in api get user with id
+@api.route('/users/<int:id>')
+def get_user(id):
+    user = User.query.get_or_404(id)
+    return jsonify(user.to_json())
+
+# get user whose id is id 's all posts
+@api.route('/users/<int:id>/posts/')
+def get_user_posts(id):
+    user = User.query.get_or_404(id)
+    page = request.args.get('page', 1, type=int)
+    pagination = user.posts.order_by(Post.timestamp.desc()).paginate(
+            page, per_page=current_app.config['BLOG_POSTS_PER_PAGE'],
+            error_out=False)
+    posts = pagination.items
+    prev = None
+    if pagination.has_prev:
+        prev = url_for('api.get_posts', page=page-1, _external=True)
+    next = None
+    if pagination.has_next:
+        next = url_for('api.get_posts', page=page+1, _external=True)
+    return jsonify({
+        'posts': [post.to_json() for post in posts],
+        'prev': prev,
+        'next': next,
+        'count': pagination.total
+        })
+
+# get user's followed 's posts
+@api.route('/users/<int:id>/timeline/')
+def get_user_followed_posts(id):
+    user = User.query.get_or_404(id)
+    page = request.args.get('page', 1, type=int)
+    pagination = user.followed_posts.order_by(Post.timestamp.desc()).paginate(
+            page, per_page=current_app.config['BLOG_POSTS_PER_PAGE'],
+            error_out=False)
+    posts = pagination.items
+    prev = None
+    if pagination.has_prev:
+        prev = url_for('api.get_posts', page=page-1, _external=True)
+    next = None
+    if pagination.has_next:
+        next = url_for('api.get_posts', page=page+1, _external=True)
+    return jsonify({
+        'posts': [post.to_json() for post in posts],
+        'prev': prev,
+        'next': next,
+        'count': pagination.total
+        })
+
--- app/models.py
+++ app/models.py
@@ -2,11 +2,12 @@ from . import db, login_manager
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask.ext.login import UserMixin, AnonymousUserMixin
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
-from flask import current_app, request
+from flask import current_app, request, url_for
 from datetime import datetime
 import hashlib
 from markdown import markdown
 import bleach
+from app.exceptions import ValidationError
 
 class Permission:
     FOLLOW = 0x01
@@ -54,6 +55,28 @@ class Post(db.Model):
 	target.body_html = bleach.linkify(bleach.clean(
 	    markdown(value, output_format='html'),
 	    tags=allowed_tags, strip=True))
+    # api
+    def to_json(self):
+        json_post = {
+                'url': url_for('api.get_post', id=self.id, _external=True),
+                'body': self.body,
+                'body_html': self.body_html,
+                'timestamp': self.timestamp,
+                'author': url_for('api.get_user', id=self.author_id,
+                                  _external=True),
+                'comments': url_for('api.get_post_comments', id=self.id,
+                                    _external=True),
+                'comments_count': self.comments.count()
+        }
+        return json_post
+
+    @staticmethod
+    def from_json(json_post):
+        body = json_post.get('body')
+        if body is None or body == '':
+            raise ValidationError('post does not have a body')
+        return Post(body=body)
+
 db.event.listen(Post.body, 'set', Post.on_changed_body)
 class Role(db.Model):
     __tablename__ = 'roles'
@@ -264,6 +287,34 @@ class User(UserMixin, db.Model):
                 db.session.commit()
             except IntegrityError:
                 db.session.rollback()
+    # api
+    def to_json(self):
+        json_user = {
+                'url': url_for('api.get_post', id=self.id, _external=True),
+                'username': self.username,
+                'member_since': self.member_since,
+                'last_seen': self.last_seen,
+                'posts': url_for('api.get_user_posts', id=self.id,
+                    _external=True),
+                'followed_posts': url_for('api.get_user_followed_posts',
+                                          id=self.id, _external=True),
+                'post_count': self.posts.count()
+        }
+        return json_user
+
+    def generate_auth_token(self, expiration):
+        s = Serializer(current_app.config['SECRET_KEY'],
+                       expires_in=expiration)
+        return s.dumps({'id': self.id}).decode('ascii')
+
+    @staticmethod
+    def verify_auth_token(token):
+        s = Serializer(current_app.config['SECRET_KEY'])
+        try:
+            data = s.loads(token)
+        except:
+            return None
+        return User.query.get(data['id'])
 
     def __repr__(self):
         return '<User %r>' % self.username
@@ -296,4 +347,25 @@ class Comment(db.Model):
         target.body_html = bleach.linkify(bleach.clean(
             markdown(value, output_format='html'),
             tags=allowed_tags, strip=True))
+
+    # api
+    def to_json(self):
+        json_comment = {
+                'url': url_for('api.get_comment', id=self.id, _external=True),
+                'post': url_for('api.get_post', id=self.post_id, _external=True),
+                'body': self.body,
+                'body_html': self.body_html,
+                'timestamp': self.timestamp,
+                'author': url_for('api.get_user', id=self.author_id,
+                                  _external=True)
+        }
+        return json_comment
+
+    @staticmethod
+    def from_json(json_comment):
+        body = json_comment.get('body')
+        if body is None or body == '':
+            raise ValidationError('comment does not have a body')
+        return Comment(body=body)
+
 db.event.listen(Comment.body, 'set', Comment.on_changed_body)
